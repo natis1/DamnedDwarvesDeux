fdacd7bdcf3ccb3ad51aafd9c2784b101edfb2e5
voidling <eli.stonium@gmail.com>
2016-04-27 13:22:35 -0600

    Added Fitness

--- src/DwarvenSpecies.java
+++ src/DwarvenSpecies.java
@@ -2,4 +2,47 @@
  * Created by scc on 4/8/2016.
  */
 public class DwarvenSpecies {
+
+    public boolean[] keysPressed = new boolean[5];
+
+    private int fitness;
+
+    public DwarvenSpecies(){
+
+    }
+
+
+    public void runProgram(){
+        for (int i = 0; i < 5; i++){
+            keysPressed[i] = false;
+        }
+
+
+
+    }
+
+    private void pushLeft(){
+
+
+
+
+
+
+    }
+    private void pushRight(){
+
+    }
+    private void pushUp(){
+
+
+    }
+    private void pushDown(){
+
+
+    }
+
+    private void shoot(){
+
+    }
+
 }
